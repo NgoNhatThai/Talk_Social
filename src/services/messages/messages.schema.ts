13f9880d348f5368e599deb9a4420a7dba59cf5e
NgoNhatThai <thai.ngonhat@idsolutions.com.vn>
2026-03-23 15:27:52 +0700

    add createAt time cho message

--- src/services/messages/messages.schema.ts
+++ src/services/messages/messages.schema.ts
@@ -17,7 +17,9 @@ export const messageSchema = Type.Object(
       file: 'file'
     }),
     replyToId: Type.Optional(ObjectIdSchema()),
-    readBy: Type.Array(ObjectIdSchema())
+    readBy: Type.Array(ObjectIdSchema()),
+    createdAt: Type.String(),
+    updatedAt: Type.String()
   },
   { $id: 'Message', additionalProperties: false }
 )
@@ -28,7 +30,7 @@ export const messageResolver = resolve<Message, HookContext>({})
 export const messageExternalResolver = resolve<Message, HookContext>({})
 
 // Schema for creating new entries
-export const messageDataSchema = Type.Pick(messageSchema, ['roomId', 'text', 'type', 'replyToId', 'senderId', 'readBy'], {
+export const messageDataSchema = Type.Pick(messageSchema, ['roomId', 'text', 'type', 'replyToId', 'senderId', 'readBy', 'createdAt', 'updatedAt'], {
   $id: 'MessageData'
 })
 export type MessageData = Static<typeof messageDataSchema>
@@ -36,18 +38,24 @@ export const messageDataValidator = getValidator(messageDataSchema, dataValidato
 export const messageDataResolver = resolve<MessageData, HookContext>({
   properties: {
     senderId: async (_value: any, _data: any, context: any) => context.params.user?._id,
-    readBy: async (_value: any, _data: any, context: any) => [context.params.user?._id]
+    readBy: async (_value: any, _data: any, context: any) => [context.params.user?._id],
+    createdAt: async () => new Date().toISOString(),
+    updatedAt: async () => new Date().toISOString()
   }
 })
 
 // Schema for updating existing entries
 export const messagePatchSchema = Type.Partial(
-  Type.Pick(messageSchema, ['readBy', 'text']),
+  Type.Pick(messageSchema, ['readBy', 'text', 'updatedAt']),
   { $id: 'MessagePatch' }
 )
 export type MessagePatch = Static<typeof messagePatchSchema>
 export const messagePatchValidator = getValidator(messagePatchSchema, dataValidator)
-export const messagePatchResolver = resolve<MessagePatch, HookContext>({})
+export const messagePatchResolver = resolve<MessagePatch, HookContext>({
+  properties: {
+    updatedAt: async () => new Date().toISOString()
+  }
+})
 
 // Schema for allowed query properties
 export const messageQueryProperties = Type.Pick(messageSchema, ['_id', 'roomId', 'senderId', 'type'])
